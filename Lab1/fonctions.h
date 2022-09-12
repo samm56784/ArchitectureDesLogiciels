01dee799db431ae4d828a835d4d2ada019e33b62
unknown <samuel.harvey890@gmail.com>
2022-09-12 12:55:18 -0400

    cleanup + replay fonctionnel

--- Lab1/fonctions.h
+++ Lab1/fonctions.h
@@ -1,10 +1,8 @@
 #pragma once
 #pragma comment(lib,"Strmiids.lib")
-#define ONE_SECOND 10000000
 #include <dshow.h>
 #include <iostream>
 using namespace std;
-//BSTR Video = SysAllocString(L"D:\\rocket.avi");
 
 bool ToucheEntrée(HRESULT, IGraphBuilder*, IMediaControl*, IMediaEvent*, IMediaSeeking*);
 
